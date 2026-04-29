8b2b9c0ece0720e38b9925a45d59f122387312b0
Leith <saddouri.leith@esprit.tn>
2026-04-29 23:02:46 +0100

    service

--- src/app/features/suggestions/core/list-suggestion/list-suggestion.component.ts
+++ src/app/features/suggestions/core/list-suggestion/list-suggestion.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Suggestion } from '../../../../models/suggestion';
+import { SuggestionService } from '../../../../services/suggestion.service';
+
 
 @Component({
   selector: 'app-list-suggestion',
@@ -10,44 +12,13 @@ export class ListSuggestionComponent {
 searchText: string = '';
 
 
-suggestions: Suggestion[] = [
-{
-id: 1,
-title: 'Organiser une journée team building',
-description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de léquipe.',
-category: 'Événements',
-date: new Date('2025-01-20'), 
-status: 'acceptee',
-nbLikes:10
-},
-{
-id: 2,
-title: 'Améliorer le système de réservation',
-description: 'Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.',
-category: 'Technologie',
-date: new Date('2025-01-15'), 
-status: 'refusee',
-nbLikes:0
-},
-{
-id: 3,
-title: 'Créer un système de récompenses',
-description: 'Mise en place dun programme de récompenses pour motiver les employés et reconnaître leurs efforts.',
-category: 'Ressources Humaines', 
-date: new Date('2025-01-25'), 
-status: 'refusee',
-nbLikes:0
-},
-{
-id: 4,
-title: 'Moderniser linterface utilisateur',
-description: 'Refonte complète de linterface utilisateur pour une meilleure expérience utilisateur.',
-category: 'Technologie',
-date: new Date('2025-01-30'), 
-status: 'en_attente',
-nbLikes:0
-},
-];
+suggestions: Suggestion[] = [];
+
+constructor(private sugS: SuggestionService) {}
+
+ngOnInit() {this.suggestions = this.sugS.suggestions;}
+
+
 
 
 products:Suggestion []= []
@@ -71,4 +42,8 @@ filteredProducts() {
   );
 
 }
+}
+
+function ngOnInit() {
+  throw new Error('Function not implemented.');
 }
--- src/app/features/suggestions/details/details.component.ts
+++ src/app/features/suggestions/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SuggestionService } from '../../../services/suggestion.service';
+import { Suggestion } from '../../../models/suggestion';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -8,11 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent {
 
   id!: number;
-constructor(private Act : ActivatedRoute) {
+  suggestion!: Suggestion;
+constructor(private Act : ActivatedRoute, private sugS: SuggestionService) {
 }
   ngOnInit() {
 this.id=this.Act.snapshot.params['id'];
-console.log(this.id);
+//this.suggestion=this.sugS.suggestions.find(sug=>sug.id==this.id)!;
+this.suggestion=this.sugS.suggestions.find(s=>s.id==this.id)!;
 }
 
 }
